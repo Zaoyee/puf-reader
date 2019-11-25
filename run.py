6ec0f19f9b260d270336386f6a040238c3c9bf61
Zaoyi <zc95@nau.edu>
2019-11-25 12:25:10 -0700

    update requirement

--- run.py
+++ run.py
@@ -3,6 +3,20 @@ import numpy as np
 import re
 import read as rder
 
+"""
+Author: Zaoyi Chi
+
+Organization: Northern Arizona University - 
+Wireless Networking and Smarth Health (WiNeSh) research laboratory
+
+Date : 11.25.2019
+
+This function reads the image dendrite optical PUFs and generate
+an unique ID in (bin, hex, dec, unit-8 char).
+
+This code is developed for educational purpose.
+"""
+
 ip, out_mode, length_required, sampleID = np.repeat(-1, 4)
 e1, e2, e3, e4 = np.repeat(0, 4)
 
@@ -22,20 +36,28 @@ im2arr = rder.PIL2ary(PILimg)
 while ip not in range(0,3):
     if e1 != 0:
         print("Unknown command, plz retype the selection mode\n")
-    ip = int(input('Choose "1" for random selection,'
-                   ' "2" for determistic selection\nInput here: '))
+    ip = int(input('Choose "1" for RANDOM selection,'
+                   ' "2" for DETERMINISTIC selection\n'
+                   '    DETERMINISTIC: extracts the same number for a dendrite in multiple readings\n'
+                   '    RANDOM: extracts different numbers for a dendrite in multiple readings\n'
+                   'Input here: '))
     if ip == 2:
-        loc = input("type in location in format *(y, x, dir)*\n"
-                    " dir :\n   0 : by row \n   1 : by column\nInput here: ")
-        loc_lst = loc.split(',')
-        loc_new = tuple([int(x) for x in loc_lst])
-        rex = re.compile('^[0-9999],[0-9999],[0-1]$')
-        while ((rex.match(loc) is None) or (rder.out_bound_check(im2arr, loc_new))):
-            loc = input("type in location in format *(y, x, dir)*\n"
-                        " dir :\n   0 : by row \n   1 : by column\n"
-                        "Input here: ")
-            loc_lst = loc.split(',')
-            loc_new = tuple([int(x) for x in loc_lst])
+        ## this part enable could make the determinstic to read from a user input location
+
+        # loc = input("type in location in format *(y, x, dir)*\n"
+        #             " dir :\n   0 : by row \n   1 : by column\nInput here: ")
+        # loc_lst = loc.split(',')
+        # loc_new = tuple([int(x) for x in loc_lst])
+        # rex = re.compile('^[0-9999],[0-9999],[0-1]$')
+        # while ((rex.match(loc) is None) or (rder.out_bound_check(im2arr, loc_new))):
+        #     loc = input("type in location in format *(y, x, dir)*\n"
+        #                 " dir :\n   0 : by row \n   1 : by column\n"
+        #                 "Input here: ")
+        #     loc_lst = loc.split(',')
+        #     loc_new = tuple([int(x) for x in loc_lst])
+
+        h, w, c = im2arr.shape
+        loc_new = (int(h/2), int(w/2), 0)
     else:
         loc_new = None
     e1 += 1
@@ -61,3 +83,4 @@ print(rder.conversion(out_mode=out_mode,
                       length_required=length_required))
 
 print('-'*40)
+PILimg.show()
